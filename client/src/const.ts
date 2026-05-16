5a1c9e97bbd14fa4eebe38a4081bb6769b9d19bb
Bia Machado <bia.x.machado@gmail.com>
2026-05-16 10:29:03 -0300

    Update const.ts

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,2 +1,2 @@
 export { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
-export const getLoginUrl = () => "/login";
+export const getLoginUrl = () => `${import.meta.env.VITE_API_URL || ""}/login`;
