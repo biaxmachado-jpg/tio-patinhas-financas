e747a46d8f8dbc6ace67e6a54b3b7b14933f7a16
Bia Machado <bia.x.machado@gmail.com>
2026-05-16 10:27:26 -0300

    Update trpc.ts

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,8 @@
-import { createTRPCReact } from "@trpc/react-query";
+import { createTRPCReact } from "@trpc/client";
 import type { AppRouter } from "../../../server/routers";
 
 export const trpc = createTRPCReact<AppRouter>();
+
+export const getApiUrl = () => {
+  return import.meta.env.VITE_API_URL || "";
+};
